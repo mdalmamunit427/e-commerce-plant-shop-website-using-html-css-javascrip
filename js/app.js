b7437814590c01aee67a7ef4a7310c15827d9b31
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-07-24 23:37:15 +0600

    mobile responsiveness

--- js/app.js
+++ js/app.js
@@ -1,36 +1,62 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', function () {
-    header.classList.toggle('sticky', window.scrollY > 0);
-})
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle("bx-x");
-    navbar.classList.toggle("open");
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
 };
 
 window.onscroll = () => {
-    menu.classList.remove("bx-x");
-    navbar.classList.remove("open"); 
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("open");
 };
 
 // review section
 
-let arrowIcon = document.getElementsByClassName('arrow-icon');
-const slide = document.getElementById('slide');
+let arrowIcon = document.getElementsByClassName("arrow-icon");
+const slide = document.getElementById("slide");
 
 arrowIcon[0].onclick = () => {
-    slide.style.transform = "translateX(0)";
-}
+  slide.style.transform = "translateX(0)";
+};
 arrowIcon[1].onclick = () => {
-    slide.style.transform = "translateX(-800px)";
-}
+  slide.style.transform = "translateX(-800px)";
+};
 arrowIcon[2].onclick = () => {
-    slide.style.transform = "translateX(-1600px)";
-}
+  slide.style.transform = "translateX(-1600px)";
+};
 arrowIcon[3].onclick = () => {
-    slide.style.transform = "translateX(-2400px)";
+  slide.style.transform = "translateX(-2400px)";
+};
+
+// media queries
+
+function myFunction(x) {
+  let arrowIcon = document.getElementsByClassName("arrow-icon");
+  const slide = document.getElementById("slide");
+  if (x.matches) {
+    // If media query matches
+    arrowIcon[0].onclick = () => {
+        slide.style.transform = "translateX(0)";
+      };
+      arrowIcon[1].onclick = () => {
+        slide.style.transform = "translateX(-380px)";
+      };
+      arrowIcon[2].onclick = () => {
+        slide.style.transform = "translateX(-760px)";
+      };
+      arrowIcon[3].onclick = () => {
+        slide.style.transform = "translateX(-1000px)";
+      };
+  } 
 }
+
+var x = window.matchMedia("(max-width: 576px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
